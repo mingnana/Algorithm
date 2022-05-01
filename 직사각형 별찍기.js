6ef05bab6033b8323c50cd160026160d1ac0731c
mi.____.ng <alstjr9438@naver.com>
2022-05-01 22:15:54 +0900

    [Programmers] 12969

---
+++ 직사각형 별찍기.js
@@ -0,0 +1,12 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    for(var i = 0; i < b; i++){
+        var answer = '';
+        for(var j = 0; j < a; j++){
+            answer += '*'
+        }
+    console.log(answer);
+    }
+});
