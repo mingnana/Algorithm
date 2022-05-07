8a47790e8d262e08efea80631cab4469d4cff0cf
mi.____.ng <alstjr9438@naver.com>
2022-05-07 22:09:04 +0900

    Update 행렬의 덧셈.js

--- 행렬의 덧셈.js
+++ 행렬의 덧셈.js
@@ -10,3 +10,17 @@ function solution(arr1, arr2) {
     }
     return answer;
 }
+
+// ***********************
+
+function solution2(arr1, arr2){
+    return arr1.map((a,i) => arr2[i].map((b,j) =>b + arr2[i][j]));
+}
+
+// ***********************
+var value = 3 // 값이 상속받는것, 스칼라
+var value = [1, 2, 3] // 배열값, 벡터
+var value = [[1,2,3] , [1,2,3]] // 배열내 배열값, 행렬(matrix)
+
+var value = [3] // 벡터
+var value = [[3]] // 행렬
