64afa6f570df30d6aa931ed48d22e9221fc1db1e
mi.____.ng <alstjr9438@naver.com>
2022-05-07 23:47:53 +0900

    [Programmers] 12935

---
+++ 제일 작은 수 제거하기.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    if(arr.length > 1){
+        var min = Math.min(...arr);
+        var idx = arr.indexOf(min);
+        //조건에 해당하는 범위중 첫번째인덱스를 선택
+        arr.splice(idx, 1)
+        // idx 자기자신을 제외시킴
+    }else{
+        arr = [-1]
+    }
+    return arr;
+}
