4c0b4d9995fcfdc2b4c5a807939e2a4d2119048d
mi.____.ng <alstjr9438@naver.com>
2022-05-07 22:52:03 +0900

    [Programmers] 12947

---
+++ 하샤드수.js
@@ -0,0 +1,18 @@
+function solution(x) {
+    var answer = true;
+    
+    var arr = String(x).split('');
+    // 문자열로 바꾼 x를 split을 통해 자릿수마다로 나눔
+    // (''): 자릿수
+    // (' ') : 단어별
+    // () : 전체
+    
+    var sum = 0
+    for(var i=0; i<arr.length; i++){
+        sum += Number(arr[i])
+    }
+  
+    var answer = x % sum === 0 ? true : false
+    // x를 sum으로 나눴을때 나머지가 0이라면 true 출력, 아니면 false 출력
+    return answer;
+}
