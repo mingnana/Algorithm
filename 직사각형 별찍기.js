4e4c5842805eec2d81f35a3ffa21d3b4ae33b43b
mi.____.ng <alstjr9438@naver.com>
2022-05-07 21:32:10 +0900

    Update 직사각형 별찍기.js

--- 직사각형 별찍기.js
+++ 직사각형 별찍기.js
@@ -10,3 +10,17 @@ process.stdin.on('data', data => {
     console.log(answer);
     }
 });
+
+// **********************************************************
+
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    
+    var row="*".repeat(a)
+    for(var i=0; i<b; i++){
+        console.log(row);
+    }
+});
