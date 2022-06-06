9c142469612f879725dbc1bbd9ffb19a76178ae1
mi.____.ng <alstjr9438@naver.com>
2022-06-06 22:42:14 +0900

    Update 제일 작은 수 제거하기.js

--- 제일 작은 수 제거하기.js
+++ 제일 작은 수 제거하기.js
@@ -11,7 +11,25 @@ function solution(arr) {
     return arr;
 }
 
-// 
+// ... arr
 var arr = [4, 3, 2];
 console.log(arr);               // [4, 3, 2]
 console.log(...arr);            // 4, 3, 2
+
+//splice
+arr.aplice(시작점, 길이, 추가요소1, 추가요소2)
+
+* 요소추가
+var num1 = ['a', 'b', 'c', 'd']
+var num2 = num1.splice(1, 0, 'f', 'g')
+    :: 배열의 index 1번 위치에서 길이가 0만큼 잘라내고, f와 g를 추가
+* 결과
+num2 = ['a', 'f', 'g', 'b', 'c', 'd']
+
+
+* 요소추가
+var num1 = ['a', 'b', 'c', 'd']
+var num2 = num1.splice(1,1)
+    :: 배열의 index 1번 위치에서 1만큼 잘라내기
+* 결과
+num2 = ['a', 'c', 'd']
