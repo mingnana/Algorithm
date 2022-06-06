395fcae9b49a9439671148dab09480aafece6d48
mi.____.ng <alstjr9438@naver.com>
2022-06-06 22:55:52 +0900

    Update 자연수 뒤집어 배열로 만들기.js

--- 자연수 뒤집어 배열로 만들기.js
+++ 자연수 뒤집어 배열로 만들기.js
@@ -6,3 +6,10 @@ function solution(n) {
     .reverse()
     .map(n => parseInt(n));
 }
+
+// map
+배열 내의 모든 요소 각각에 대해 주어진 함수를 호출한 결과는 모아 새로운 배열로 반환함
+var array = [1, 4, 7, 10]
+var map = array.map(x => x*2);
+console.log(map)
+    :: array [2, 8, 14, 20] 반환 
