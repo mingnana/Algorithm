560dd174bfea51e67e74489b771ec75f98b4f852
mi.____.ng <alstjr9438@naver.com>
2022-06-07 16:24:51 +0900

    Update 자릿수더하기.js

--- 자릿수더하기.js
+++ 자릿수더하기.js
@@ -25,3 +25,14 @@ function solution2(n)
 
     return answer;
 }
+
+//배열.reduce((누적값, 현잿값, 인덱스, 요소) => {return 결과}, 초기값);
+const oneTwoThree = [1, 2, 3];
+result = oneTwoThree.reduce((acc, cur, i) => {
+  console.log(acc, cur, i);
+  return acc + cur;
+}, 0);
+// 0 1 0
+// 1 2 1
+// 3 3 2
+result; // 6
