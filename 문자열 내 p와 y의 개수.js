d426c64df1fa21ceb046eb0034d47cf1a8e39c8e
mingnana <alstjr9438@naver.com>
2022-06-07 22:18:21 +0900

    [Programmers] 12916

---
+++ 문자열 내 p와 y의 개수.js
@@ -0,0 +1,12 @@
+function solution(s){
+    var answer = true;
+
+    var p = s.toLowerCase().split("p").length;
+    var y = s.toLowerCase().split("y").length;
+    
+    return p == y ? true : false
+
+    // 문자열에 특정문자의 개수를 구하려면 split을 사용하면됨
+    // "ababb".split("a") 의 결과는 ["", "b", "bb"]
+    // 즉, "a"의 갯수는 3에서 1을 뺀 2
+}
