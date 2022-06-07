7ff22ab3314ec30e99d8f22038550deb5c8eb484
mingnana <alstjr9438@naver.com>
2022-06-07 19:02:48 +0900

    [Programmers] 12931

--- 자릿수더하기.js
+++ 자릿수더하기.js
@@ -13,7 +13,7 @@ function solution1(n)
 }
 
 
-// 풀이2
+//
 function solution2(n)
 {
     var answer = 0;
@@ -25,27 +25,3 @@ function solution2(n)
 
     return answer;
 }
-
-// 풀이법3
-// 출처 : https://github.com/codeisneverodd/programmers-coding-test/blob/main/level-1/%EC%9E%90%EB%A6%BF%EC%88%98-%EB%8D%94%ED%95%98%EA%B8%B0.js
-function solution3(n){
-    var answer = 0;
-
-    n.toString()
-        .split('')
-        .forEach((n) => {
-        answer += parseInt(n)
-    })
-    return answer;
-}
-
-//배열.reduce((누적값, 현잿값, 인덱스, 요소) => {return 결과}, 초기값);
-const oneTwoThree = [1, 2, 3];
-result = oneTwoThree.reduce((acc, cur, i) => {
-  console.log(acc, cur, i);
-  return acc + cur;
-}, 0);
-// 0 1 0
-// 1 2 1
-// 3 3 2
-result; // 6
