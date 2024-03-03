cc2f0dad70b9a333ee87d4b0ffc842d81e55bd08
LAPTOP-JN3UFLM6\user <alstjr9438@naver.com>
2024-03-04 00:06:34 +0900

    while문, 할당연산자

--- 0단계/피자 나눠 먹기.js
+++ 0단계/피자 나눠 먹기.js
@@ -1,3 +1,31 @@
+// 오답 코드
+// slice-n 이 음수일때만 answer를 증가 시키고 있지만 slice가 더 큰 경우에도 피자를 시켜야 할 필요가 있음
+// 반복문 안에서 break를 사용하는데, 반복문을 한번만 실행하고 끝내는 것으로 slice가 n보다 클 경우 바로 반복문 종료함
+function solution(slice, n) {
+    var answer = 0;
+    while (slice - n < 0) {
+        answer++;
+        if (slice - n > 0) {
+            answer++;
+            break;
+        }
+    }
+    return answer;
+}
+
+// 다른 풀이
+// slice 가 0보다 클때까지 반복하며, 각 반복마다 slice - n 처리
+// anser의 횟수를 증가함
+
+function solution(slice, n) {
+    var answer = 0;
+    while (slice > 0) {
+        slice -= n;
+        answer++;
+    }
+    return answer;
+}
+// 다른 풀이
 function solution(slice, n) {
     var answer = 0;
 
